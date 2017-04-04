944f8644c8495f1107d9308c165dbda9d771fdbb
phrz <phrz@me.com>
2017-04-04 10:12:52 -0500

    Fixed gulp, html, typescript tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,43 @@
 
 const gulp = require('gulp');
 const del = require('del');
+const browserSync = require('browser-sync').create();
+const ts = require('gulp-typescript');
+
+let build_dir = './build/';
+let source_dir = './src/';
 
 gulp.task('html', () => {
-	gulp.src('/**/*.html', { base: 'src' })
-	.pipe(gulp.dest('build'));
+	gulp.src(source_dir + '**/*.html', { base: source_dir })
+	.pipe(gulp.dest(build_dir));
+});
+
+gulp.task('ts', () => {
+	let project = ts.createProject('tsconfig.json');
+	let tsc = gulp
+		.src(source_dir + '**/*.ts', { base: source_dir })
+		.pipe(project())
+		.pipe(gulp.dest(build_dir));
 });
 
 gulp.task('bootstrap', () => {
-	gulp.src('/**/*.*', { base: 'node_modules/bootstrap/dist' })
-	.pipe(gulp.dest('build'));
-});
-
-gulp.task('config', () => {
-	gulp.src(['**/*.json'])
+	gulp.src('./node_modules/bootstrap/dist/**/*.*', { base: 'node_modules/bootstrap/dist' })
+	.pipe(gulp.dest(build_dir + 'vendor/bootstrap'));
 });
 
 gulp.task('clean', () => {
-	return del(['build/*']);
+	return del([build_dir + '*']);
 });
 
-gulp.task('serve', () => {
-
+gulp.task('sync', () => {
+	browserSync.init({
+		server: {
+			baseDir: build_dir
+		}
+	});
 });
 
-gulp.task('build', ['html', 'config', 'bootstrap']);
-gulp.task('run', ['build', 'serve'])
+gulp.task('build', ['html', 'ts', 'bootstrap']);
+gulp.task('run', ['clean', 'build', 'sync']);
 
 gulp.task('default', ['build']);
